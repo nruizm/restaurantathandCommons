60084b85afe40fb6bc306a4343b26f967b403b89
natip <natip@192.168.0.11>
2020-10-15 12:08:23 -0500

    Se crearon las clases person, menu, restaurant. La clase menu solo fue definida

---
+++ src/main/java/co/unicauca/restaurantathand/commons/infra/Parameter.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.unicauca.restaurantathand.commons.infra;
+
+/**
+ * Parametro.
+ * Lo usa la clase Protocol
+ * @author Libardo, Julio
+ */
+public class Parameter {
+
+    private String name;
+    private String value;
+
+    public Parameter(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public Parameter() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
