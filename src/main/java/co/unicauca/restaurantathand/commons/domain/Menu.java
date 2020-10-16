477285ff1bc295d4fbf90c2c12d39df3a5908535
lenovo <lenovo@LAPTOP-ON309EGL>
2020-10-16 00:48:09 -0500

    Se implemento la clase menu

--- src/main/java/co/unicauca/restaurantathand/commons/domain/Menu.java
+++ src/main/java/co/unicauca/restaurantathand/commons/domain/Menu.java
@@ -1,14 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.unicauca.restaurantathand.commons.domain;
 
+import java.util.Date;
+import java.util.List;
+
 /**
- *
- * @author lenovo
+ *  Menu de un Restaurante 
+ * 
+ * @author Mannuel Fernando Granoble
+ *         Michel Andrea Gutierrez Vallejo
+ *         Ximena Quijano Gutierrez
+ *         Nathalia Ruiz Menses
  */
 public class Menu {
+    
+    private String atrIdMenu;
+    private String atrNomMenu;
+    private Date atrFecVimenu;
+    private List<Dish> atrlistaPlatos;
+    private String atrTypeMenu;
+
+    public Menu(String atrIdMenu, String atrNomMenu, Date atrFecVimenu, String atrTypeMenu) {
+        this.atrIdMenu = atrIdMenu;
+        this.atrNomMenu = atrNomMenu;
+        this.atrFecVimenu = atrFecVimenu;
+        this.atrTypeMenu = atrTypeMenu;
+    }
+
+    public Menu() {
+        
+    }
+
+    public String getAtrTypeMenu() {
+        return atrTypeMenu;
+    }
+
+    public void setAtrTypeMenu(String atrTypeMenu) {
+        this.atrTypeMenu = atrTypeMenu;
+    }
+ 
+    
+    
+    public String getAtrIdMenu() {
+        return atrIdMenu;
+    }
+
+    public void setAtrIdMenu(String atrIdMenu) {
+        this.atrIdMenu = atrIdMenu;
+    }
+
+    public String getAtrNomMenu() {
+        return atrNomMenu;
+    }
+
+    public void setAtrNomMenu(String atrNomMenu) {
+        this.atrNomMenu = atrNomMenu;
+    }
+
+    public Date getAtrFecVimenu() {
+        return atrFecVimenu;
+    }
+
+    public void setAtrFecVimenu(Date atrFecVimenu) {
+        this.atrFecVimenu = atrFecVimenu;
+    }
+
+    public List<Dish> getAtrlistaPlatos() {
+        return atrlistaPlatos;
+    }
+
+    public void setAtrlistaPlatos(Dish prmPlatos) {
+        atrlistaPlatos.add(prmPlatos);
+    }
+    
+    
     
 }
